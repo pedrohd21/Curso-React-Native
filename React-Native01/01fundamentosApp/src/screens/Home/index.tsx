db222879134cdac84214c03f225960d0aff1c7db
Pedro Henrique <67549131+pedrohd21@users.noreply.github.com>
2023-05-24 22:19:56 -0300

    Atualizar

--- React-Native01/01fundamentosApp/src/screens/Home/index.tsx
+++ React-Native01/01fundamentosApp/src/screens/Home/index.tsx
@@ -64,7 +64,7 @@ export default function Home() {
         showsVerticalScrollIndicator={false}
         ListEmptyComponent={() => (
           <Text style={styles.listEmptyText}>
-            ninguem chegou aq
+            ninguem chegou aqui.
           </Text>
         )}
       /> 
